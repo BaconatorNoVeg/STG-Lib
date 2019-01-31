5455d782597fe13702110c8efa9af7d3a0b6102a
Joshua Luce <BaconatorNoVeg@users.noreply.github.com>
2019-01-31 15:21:23 -0600

    Finished writing unit test

--- src/test/java/STGLibTest.java
+++ src/test/java/STGLibTest.java
@@ -1,24 +1,22 @@
-import com.baconatornoveg.stglib.God;
 import com.baconatornoveg.stglib.Player;
 import com.baconatornoveg.stglib.SmiteTeamGenerator;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 import static junit.framework.TestCase.fail;
 
 public class STGLibTest {
 
-    SmiteTeamGenerator stg;
-    ArrayList<String> gods;
-    ArrayList<String> items;
-    int godsInitialSize;
-    int itemsInitialSize;
+    private SmiteTeamGenerator stg;
+    private ArrayList<String> gods;
+    private ArrayList<String> items;
+    private int godsInitialSize;
+    private int itemsInitialSize;
 
     public STGLibTest() {
         Scanner in;
@@ -55,6 +53,12 @@ public class STGLibTest {
         }
         stg = new SmiteTeamGenerator();
         stg.getLists(true);
+        stg.isForcingOffensive = true;
+        stg.isForcingDefensive = true;
+        stg.isForcingBalanced = true;
+        System.out.println("Forcing Offensive: " + stg.isForcingOffensive);
+        System.out.println("Forcing Defensive: " + stg.isForcingDefensive);
+        System.out.println("Forcing Balanced " + stg.isForcingBalanced);
 
     }
 
@@ -80,12 +84,12 @@ public class STGLibTest {
             }
         }
         if (!(gods.size() <= 0)) {
-            System.err.println("Failed to confirm all " + itemsInitialSize + " items being generated in " + maxTries + " total generations.");
-            System.err.println("Leftover items: " + items.toString());
+            System.err.println("Failed to confirm all " + godsInitialSize + " gods being generated in " + maxTries + " total generations.");
+            System.err.println("Leftover gods: " + gods.toString());
             fail();
         } else {
-            System.out.println("Successfully confirmed all " + itemsInitialSize + " items being generated in " + totalAttempts + " generations.");
-            System.out.println("Leftover items: " + items.toString());
+            System.out.println("Successfully confirmed all " + godsInitialSize + " gods being generated in " + totalAttempts + " generations.");
+            System.out.println("Leftover gods: " + gods.toString());
         }
     }
 
@@ -93,29 +97,54 @@ public class STGLibTest {
     public void testForAllItems() {
         int maxTries = 1000000;
         int totalAttempts = 0;
-
+        ArrayList<String> testedItems = new ArrayList<>();
+        ArrayList<String> missingItems = items;
         for (int i = 0; i < maxTries; i++) {
             totalAttempts++;
             Player player = stg.makeLoadout(SmiteTeamGenerator.Positions.values()[(int)(Math.random() * 5)]);
             ArrayList<String> build = player.getBuild();
             for (String item : items) {
                 for (String itm : build) {
-                    if (itm.equals(item)) {
-                        items.remove(item);
+                    if (itm.equals(item) && !testedItems.contains(item)) {
+                        testedItems.add(item);
                     }
                 }
             }
-            if (items.size() <= 0) {
-                break;
+            Collections.sort(items);
+            Collections.sort(testedItems);
+            if (items.size() == testedItems.size()) {
+                if (items.equals(testedItems)) {
+                    break;
+                }
             }
         }
-        if (!(items.size() <= 0)) {
-            System.err.println("Failed to confirm all " + godsInitialSize + " gods being generated in " + maxTries + " total generations.");
-            System.err.println("Leftover gods: " + gods.toString());
+        missingItems.removeAll(testedItems);
+        if (!(itemsInitialSize == testedItems.size()) && !(missingItems.size() == 0)) {
+            System.err.println("Failed to confirm all " + itemsInitialSize + " items being generated in " + maxTries + " total generations.");
+            System.err.println(testedItems.size() + " items succeeded out of " + itemsInitialSize);
+            System.err.println("Tested items: \n" + testedItems.toString());
+            System.err.println("Missing items: \n" + missingItems.toString());
             fail();
         } else {
-            System.out.println("Successfully confirmed all " + godsInitialSize + " gods being generated in " + totalAttempts + " generations.");
-            System.out.println("Leftover gods: " + gods.toString());
+            System.out.println("Successfully confirmed all " + itemsInitialSize + " items being generated in " + totalAttempts + " generations.");
+            System.out.println("Tested items: \n" + testedItems.toString());
+            System.out.println("Missing items: \n" + missingItems.toString());
+        }
+    }
+
+    @Test
+    public void testForKatanasOnHunters() {
+        int maxTries = 1000000;
+        int totalAttempts = 0;
+        for (int i = 0; i < maxTries; i++) {
+            totalAttempts++;
+            Player player = stg.makeLoadout(SmiteTeamGenerator.Positions.HUNTER);
+            ArrayList<String> build = player.getBuild();
+            if (build.contains("Hastened Katana") || build.contains("Masamune") || build.contains("Stone Cutting Sword") || build.contains("Golden Blade")) {
+                System.err.println("A hunter build contains a forbidden hunter item.");
+                System.err.println("Build: " + player.toString());
+                fail("A hunter build contains a forbidden hunter item.");
+            }
         }
     }
 
