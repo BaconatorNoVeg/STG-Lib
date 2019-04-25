0b83fc7cf587688179b110d7ceb33b99e92c32df
Joshua Luce <joshuabluce@gmail.com>
2019-04-25 02:12:53 -0500

    Added methods to check if god is physical or magical

--- src/main/java/com/baconatornoveg/stglib/God.java
+++ src/main/java/com/baconatornoveg/stglib/God.java
@@ -1,13 +1,25 @@
 package com.baconatornoveg.stglib;
 
+import java.util.Arrays;
+
 public class God {
 
     private String name;
     private String position;
+    private String[] physicals = {"Assassin", "Hunter", "Warrior"};
+    private String[] magicals = {"Mage", "Guardian"};
 
     protected God(String name, String position) {
         this.name = name;
         this.position = position;
+    }
+
+    public boolean isPhysical() {
+        return Arrays.asList(physicals).contains(position);
+    }
+
+    public boolean isMagical() {
+        return Arrays.asList(magicals).contains(position);
     }
 
     public String getName() {
